12e44313c60f7851f340dedd13eec4303bf3c8e9
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-06-19 16:10:58 +0530

    assignment button

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
 import React from "react";
 import './../styles/App.css';
+import {useState} from "react";
 
 const App = () => {
+
+  let [count, setCount] = useState(0)
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p style={{fontFamily:"bold"}}>Button clicked {count} times</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>Click me</button>
     </div>
   )
 }
